c4a380273cdb128ca26cb7dc0ea621de402fb047
Sathvara-Amitkumar <sathvaraamit2005@gmail.com>
2026-08-09 17:25:45 +0530

    Use http metho

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -8,3 +8,12 @@ def home():
         "message": "Hello, PY coder!",
         "error":"i'm stuck."
     }
+
+@app.get("/products/{id}")
+def get_products(id: int):
+    products = ['Mobile', 'Laptop', 'TV', 'Computer', 'Chair', 'Table', 'PAD']
+
+    if id < 0 or id >= len(products):
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return products[id]
