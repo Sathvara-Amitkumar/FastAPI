390962e7aee3f46223ed211a9d6377123b675e1d
Sathvara-Amitkumar <sathvaraamit2005@gmail.com>
2026-09-07 11:17:24 +0530

    Use Render database environment variable

--- app/config/config.py
+++ app/config/config.py
@@ -1,6 +1,22 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+import os
 
-db_url = "postgresql+psycopg2://postgres:123@localhost:2580/project_fastapi"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+db_url = os.getenv("DATABASE_URL")
+
+if not db_url:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
+# Render gives postgres:// or postgresql://
+# SQLAlchemy + psycopg2 should use postgresql+psycopg2://
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql+psycopg2://",1)
+
+elif db_url.startswith("postgresql://"):
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg2://", 1)
+
 engine = create_engine(db_url)
+
 session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
